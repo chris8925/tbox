ea939d9464a0c2d3f51c0745a7a5256bb5296404
chris8925 <chris1994dattenberg@gmail.com>
2026-02-23 21:28:24 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'tbox-pwa-final-v1';
+const FILES = ['./','./index.html'];
+
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)));
+});
+
+self.addEventListener('activate', e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(
+      keys.map(k=>k!==CACHE?caches.delete(k):null)
+    ))
+  );
+});
+
+self.addEventListener('fetch', e=>{
+  e.respondWith(
+    caches.match(e.request).then(res=>res||fetch(e.request))
+  );
+});
